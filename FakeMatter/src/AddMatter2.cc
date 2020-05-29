6f3a51d3d3a9c13082de9220d41138ff9ba2adfc
Qingwen Wang <qwang@mn001.cm.cluster>
2020-05-29 12:18:17 -0400

    add matter with smmoothing method at the origin

--- FakeMatter/src/AddMatter2.cc
+++ FakeMatter/src/AddMatter2.cc
@@ -9,42 +9,6 @@ using namespace std;
 
 
 
-// Matrix determinant
-static CCTK_REAL determinant(const CCTK_REAL (&g)[3][3]) {
-  return g[0][0] * (g[1][1] * g[2][2] - g[1][2] * g[2][1]) -
-         g[1][0] * (g[0][1] * g[2][2] - g[0][2] * g[2][0]) +
-         g[2][0] * (g[0][1] * g[1][2] - g[0][2] * g[1][0]);
-}
-
-// Matrix inverse
-static void inverse(const CCTK_REAL (&g)[3][3], const CCTK_REAL detg,
-                    CCTK_REAL (&gu)[3][3]) {
-  gu[0][0] = (g[1][1] * g[2][2] - g[1][2] * g[2][1]) / detg;
-  gu[1][0] = (g[0][1] * g[2][2] - g[0][2] * g[2][1]) / detg;
-  gu[2][0] = (g[0][1] * g[1][2] - g[0][2] * g[1][1]) / detg;
-  gu[1][1] = (g[0][0] * g[2][2] - g[0][2] * g[2][0]) / detg;
-  gu[2][1] = (g[0][0] * g[1][2] - g[0][2] * g[1][0]) / detg;
-  gu[2][2] = (g[0][0] * g[1][1] - g[0][1] * g[1][0]) / detg;
-  gu[0][1] = gu[1][0];
-  gu[0][2] = gu[2][0];
-  gu[1][2] = gu[2][1];
-}
-
-// Length of a vector
-static CCTK_REAL length(const CCTK_REAL (&g)[3][3], const CCTK_REAL (&x)[3]) {
-  CCTK_REAL len = 0;
-  for (int a = 0; a < 3; ++a)
-    for (int b = 0; b < 3; ++b)
-      len += g[a][b] * x[a] * x[b];
-  len = sqrt(len);
-  return len;
-}
-
-// Normalize a vector (to unit length)
-static void normalize(CCTK_REAL (&x)[3], CCTK_REAL len) {
-  for (int a = 0; a < 3; ++a)
-    x[a] /= len;
-}
 
 extern "C" void FakeMatter_AddMatter2(CCTK_ARGUMENTS) {
   DECLARE_CCTK_ARGUMENTS;
@@ -55,34 +19,49 @@ extern "C" void FakeMatter_AddMatter2(CCTK_ARGUMENTS) {
      for (int i = 0; i < cctk_lsh[0]; ++i) {
         int ijk = CCTK_GFINDEX3D(cctkGH, i, j, k);
 
-
-          const CCTK_REAL g[3][3] = {{gxx[ijk], gxy[ijk], gxz[ijk]},
-                                     {gxy[ijk], gyy[ijk], gyz[ijk]},
-                                     {gxz[ijk], gyz[ijk], gzz[ijk]}};
-          // Metric determinant
-          const CCTK_REAL detg = determinant(g);
-          // Inverse metric
-          CCTK_REAL gu[3][3];
-          inverse(g, detg, gu);
-	  
-        double rho1 = r[ijk];
-        if(rho1<1e-1)rho1=0.1;
-
         double xx=x[ijk];
         double zz=z[ijk];
         double yy=y[ijk];
+     
+        double rho1 = sqrt(pow(xx,2)+pow(yy,2))+pow(zz,2);
+     //   double rho2= sqrt(pow(xx,2)+pow(yy,2));
+     //   if(rho2<1e-1)rho2=0.1;
 
-
-          double g11= - 8* param_turnon * param_a * param_m /  rho1*(pow(param_m,2)+4*(-1+2*param_a)*param_m*rho1+4*pow(rho1,2));
-          double g22= 4*param_turnon* param_a * param_m * rho1 * ( pow(param_m,2) + 4 * param_a * param_m * rho1 + 4* pow(rho1,2))/ pow( pow(param_m,2) + 4 * (-1+ 2 * param_a) * param_m *rho1+4 * pow(rho1,2),2) ;
+  double g112= (pow(param_m,2)+4*(-1+2*param_a)*param_m*rho1+4*pow(rho1,2));
+  double g111= - 8* param_turnon * param_a * param_m ;
+  double g11=g111/g112;
+  double g22= 4*param_turnon* param_a * param_m * rho1 * ( pow(param_m,2) + 4 * param_a * param_m * rho1 + 4* pow(rho1,2))/ pow( pow(param_m,2) + 4 * (-1+ 2 * param_a) * param_m *rho1+4 * pow(rho1,2),2) ;
           
-          double s1=sqrt(pow(xx,2)+pow(yy,2))/sqrt(pow(xx,2)+pow(yy,2)+pow(zz,2)); 
-          double c1=zz/sqrt(pow(xx,2)+pow(yy,2)+pow(zz,2));
-          double s2=xx/sqrt(pow(xx,2)+pow(yy,2));
-          double c2=yy/sqrt(pow(xx,2)+pow(yy,2)); 
- if(s1<1e-3)s1=1e-3;
-          double trans[4][4] = {{1,0,0,0},{0,s1*s2,c2*s1,c1},{0,c1*s2/rho1,c1*c2/rho1,-s1/rho1},{0,c2/(rho1*s1),-s2/(rho1*s1),0}};
-          double Trho[4][4]  = {{0,0,0,0},{0,g11,0,0},{0,0,g22,0},{0,0,0,g22*pow(s1,2)}};
+
+          eTxx[ijk] += (g22*(pow(zz,2) + pow(yy,2))+g11*pow(xx,2)*rho1)/(pow(rho1,4)+0.01);
+          eTyy[ijk] += (g22*(pow(xx,2) + pow(zz,2))+g11*pow(yy,2)*rho1)/(pow(rho1,4)+0.01);
+          eTzz[ijk] += (g22*(pow(xx,2) + pow(yy,2))+g11*pow(zz,2)*rho1)/(pow(rho1,4)+0.01);
+          eTxy[ijk] += xx *yy *(-g22 + g11*rho1)/(pow(rho1,4)+0.01);
+          eTxz[ijk] += xx *zz *(-g22 + g11*rho1)/(pow(rho1,4)+0.01);
+          eTyz[ijk] += zz *yy *(-g22 + g11*rho1)/(pow(rho1,4)+0.01) ;          
+  /*
+     
+  double g112= rho1*(pow(param_m,2)+4*(-1+2*param_a)*param_m*rho1+4*pow(rho1,2));
+  double g111= - 8* param_turnon * param_a * param_m ;
+  double g11=g111/g112;
+          double g22= 4*param_turnon* param_a * param_m * rho1 * ( pow(param_m,2) + 4 * param_a * param_m * rho1 + 4* pow(rho1,2))/ pow( pow(param_m,2) + 4 * (-1+ 2 * param_a) * param_m *rho1+4 * pow(rho1,2),2) ;
+    if(rho1<1e-1)rho1=0.1;
+          eTxx[ijk] += (g22*(pow(zz,2) + pow(yy,2)))/pow(rho1,4) + g11*pow(xx,2)/pow(rho1,2) ;
+          eTyy[ijk] += (g22*(pow(xx,2) + pow(zz,2)))/pow(rho1,4) + g11*pow(yy,2)/pow(rho1,2) ;
+          eTzz[ijk] += (g22*(pow(xx,2) + pow(yy,2)))/pow(rho1,4) + g11*pow(zz,2)/pow(rho1,2) ;
+          eTxy[ijk] += xx *yy *(-g22 + g11*pow(rho1,2))/pow(rho1,4) ;
+          eTxz[ijk] += xx *zz *(-g22 + g11*pow(rho1,2))/pow(rho1,4) ;
+          eTyz[ijk] += zz *yy *(-g22 + g11*pow(rho1,2))/pow(rho1,4) ;
+
+
+*/
+/*
+          double s1=sqrt(pow(xx,2)+pow(yy,2))/rho1; 
+          double c1=zz/rho1;
+          double s2=xx/rho2;
+          double c2=yy/rho2; 
+          double trans[4][4] = {{1,0,0,0},{0,s1*s2,c2*s1,c1},{0,c1*s2/rho1,c1*c2/rho1,-s1/rho1},{0,c2/(rho1),-s2/(rho1),0}};
+          double Trho[4][4]  = {{0,0,0,0},{0,g11,0,0},{0,0,g22,0},{0,0,0,g22}};
           double Txyz[4][4];
     
           for (int a = 0; a<4; ++a) 
@@ -93,15 +72,28 @@ extern "C" void FakeMatter_AddMatter2(CCTK_ARGUMENTS) {
                       Txyz[a][b] += trans[c][a]* Trho[c][d]* trans[d][b];
           }
           
-          
           eTxx[ijk] += Txyz[1][1];
           eTyy[ijk] += Txyz[2][2];
           eTzz[ijk] += Txyz[3][3];
           eTxy[ijk] += Txyz[1][2];
           eTxz[ijk] += Txyz[1][3];
-          eTyz[ijk] += Txyz[2][3]; 
+          eTyz[ijk] += Txyz[2][3];
+  
+*/
+
+        
+  //        eTxx[ijk] += 0;
+  //        eTyy[ijk] += 0;
+  //        eTzz[ijk] += 0;
+  //        eTxy[ijk] += 0;
+  //        eTxz[ijk] += 0;
+  //        eTyz[iji] += 0; 
        
-   if(isnan(eTxx[ijk])){printf("nan in eTxx at %g,%g,%g,%g,%g,%g,%g\n",x[ijk],y[ijk],z[ijk], s1,s2,c1,c2);}
+
+
+
+//if(x[ijk]==0&&y[ijk]==0&&z[ijk]==0){printf("at origin i%d,j%d,k%d,x%g,y%g,z%g,eTxx%g,eTyy%g,eTzz%g,eTxy%g,eTyz%g,eTxz%g,g111%g,g1112%g,g11%g,g22%g,rho%g\n",i,j,k,x[ijk],y[ijk],z[ijk],eTxx[ijk],eTyy[ijk],eTzz[ijk],eTxy[ijk],eTyz[ijk],eTxz[ijk],g111, g112,g11,g22,rho1);}
+
 
        }
     }
--- FakeMatter/src/DefineMetric.cc
+++ FakeMatter/src/DefineMetric.cc
@@ -47,8 +47,14 @@ for (int k = 0; k < cctk_lsh[2]; ++k) {
   if(!isfinite(gyz[ijk])){printf("inf/ind in gyz at %d,%d,%d,%g,%g,%g\n",i,j,k,x[ijk],y[ijk],z[ijk]);}
   if(!isfinite(alp[ijk])){printf("inf/ind in alp at %d,%d,%d,%g,%g,%g\n",i,j,k,x[ijk],y[ijk],z[ijk]);}
 
-  if(x[ijk]==0&&y[ijk]==0&&z[ijk]==0){printf("metric gxx, gyy, gzz, gxy, gyz, gxz, alp, rho, psi4 at origin %d,%d,%d,%g,%g,%g,%g,%g,%g,%g,%g,%g,%g\n",i,j,k,x[ijk],y[ijk],z[ijk],gxx[ijk],gyy[ijk],gzz[ijk],gxy[ijk],gyz[ijk],gxz[ijk],alp[ijk],rho1,psi4);}
+//  if(x[ijk]==0&&y[ijk]==0&&z[ijk]==0){printf("metric gxx, gyy, gzz, gxy, gyz, gxz, alp, rho, psi4 at origin %d,%d,%d,%g,%g,%g,%g,%g,%g,%g,%g,%g,%g\n",i,j,k,x[ijk],y[ijk],z[ijk],gxx[ijk],gyy[ijk],gzz[ijk],gxy[ijk],gyz[ijk],gxz[ijk],alp[ijk],rho1,psi4);}
 
+  double g112= rho1*(pow(param_m,2)+4*(-1+2*param_a)*param_m*rho1+4*pow(rho1,2));
+  double g111= - 8* param_turnon * param_a * param_m ;
+  double g11=g111/g112;
+          double g22= 4*param_turnon* param_a * param_m * rho1 * ( pow(param_m,2) + 4 * param_a * param_m * rho1 + 4* pow(rho1,2))/ pow( pow(param_m,2) + 4 * (-1+ 2 * param_a) * param_m *rho1+4 * pow(rho1,2),2) ;
+
+if(x[ijk]==0&&y[ijk]==0&&z[ijk]==0){printf("at origin i%d,j%d,k%d,x%g,y%g,z%g,eTxx%g,eTyy%g,eTzz%g,eTxy%g,eTyz%g,eTxz%g,g111%g,g1112%g,g11%g,g22%g,rho%g\n",i,j,k,x[ijk],y[ijk],z[ijk],eTxx[ijk],eTyy[ijk],eTzz[ijk],eTxy[ijk],eTyz[ijk],eTxz[ijk],g111, g112,g11,g22,rho1);}
 }
 }
 }
